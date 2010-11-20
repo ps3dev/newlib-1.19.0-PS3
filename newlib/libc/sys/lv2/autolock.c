3355ece39e27f87c495733c5c064b26802727e2a
Marcus Comstedt <marcus@mc.pp.se>
2010-11-20 14:46:11 +0100

    Use gcc constructor to init metalock
    Creating the metaLock from a gcc constructor fixes any remaining
    race condition, since the constructor is called before any code
    using locks gets a chance to run.  Just remember to not put any
    initializers using malloc et al at level 105 or lower.  :-)

--- newlib/libc/sys/lv2/autolock.c
+++ newlib/libc/sys/lv2/autolock.c
@@ -11,15 +11,19 @@
 #include "lock-internal.h"
 
 
-static sys_lwmutex_t metaLock = { 0, };
+static sys_lwmutex_t metaLock;
+
+static void init_metalock(void) __attribute__((constructor(105)));
+static void init_metalock(void)
+{
+  if (sys_lwmutex_create(&metaLock, &__libc_lock_attributes))
+    abort();
+}
 
 void __libc_auto_lock_allocate(sys_lwmutex_t *pMutex)
 {
-  if (LWMUTEX_UNINITIALIZED(&metaLock)) {
-    /* FIXME: There is a potential race condition here... */
-    sys_lwmutex_create(&metaLock, &__libc_lock_attributes);
-  }
-  sys_lwmutex_lock(&metaLock, 0);
+  if (sys_lwmutex_lock(&metaLock, 0))
+    abort();
 
   /* Need to check the magic again now that we have the lock... */
   if (LWMUTEX_UNINITIALIZED(pMutex)) {
@@ -28,6 +32,7 @@ void __libc_auto_lock_allocate(sys_lwmutex_t *pMutex)
 
   }
 
-  sys_lwmutex_unlock(&metaLock);
+  if (sys_lwmutex_unlock(&metaLock))
+    abort();
 }
 
